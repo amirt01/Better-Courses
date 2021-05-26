b2b5e524019f2747b198e5625b7f5f676d78df32
amirt01 <amirtadros01@gmail.com>
2021-05-26 11:02:11 -0700

    full catalog and course_demo

--- catalog.py
+++ catalog.py
@@ -3,19 +3,22 @@ import jsons
 from course import Course
 
 
-class Catalog(set):
+class Catalog(dict):
     def __init__(self, path: str = '', *args, **kwargs):
         super().__init__(*args, **kwargs)
         if path:
-            self.read_from_file(path)
+            self.load_from_file(path)
+
+    def add(self, course: Course):
+        self[course.section + ' ' + str(course.code)] = course
 
     def save_to_file(self, path: str):
         with open(path, 'w') as outfile:
             outfile.write(jsons.dumps(self, strip_privates=True))
 
-    def read_from_file(self, path: str):
+    def load_from_file(self, path: str):
         with open(path, 'r') as infile:
-            for course in jsons.loads(infile.read(), cls=list[Course]):
+            for course in jsons.loads(infile.read(), cls=dict[str, Course]).values():
                 self.add(course)
 
     def __str__(self):
--- course.py
+++ course.py
@@ -1,23 +1,18 @@
 class Course:
-    def __init__(self, section: str = '', code=0, title: str = '', units: int = 0, description: str = '',
-                 design_units: int = 0, corequisites: list = None, prerequisites: list = None):
-        self._section: str = str(section)
+    def __init__(self, section='', code=0, title='', units=0, description='', corequisites=None, prerequisites=None):
+        self._section = str(section)
         try:
-            self._code: int = int(code)
+            self._code = int(code)
         except ValueError:
-            self._code: str = str(code)
-        self._title: str = str(title)
+            self._code = str(code)
+        self._title = str(title)
         try:
-            self._units: int = int(units)
+            self._units = int(units)
         except ValueError:
-            self._units: str = str(units)
-        self._description: str = str(description)
-        try:
-            self._design_units: int = int(design_units)
-        except ValueError:
-            self._design_units: str = str(design_units)
-        self._corequisites: list[str] = corequisites if corequisites else []
-        self._prerequisites: list[str] = prerequisites if prerequisites else []
+            self._units = str(units)
+        self._description = str(description)
+        self._corequisites = corequisites if corequisites else []
+        self._prerequisites = prerequisites if prerequisites else []
 
     @property
     def section(self):
@@ -38,10 +33,6 @@ class Course:
     @property
     def description(self):
         return self._description
-
-    @property
-    def design_units(self):
-        return self._design_units
 
     @property
     def corequisites(self):
@@ -70,10 +61,6 @@ class Course:
     @description.setter
     def description(self, new_description):
         self._description = new_description
-
-    @design_units.setter
-    def design_units(self, new_design_units):
-        self._design_units = new_design_units
 
     def add_corequisite(self, corequisite):
         self._corequisites.append(corequisite)
